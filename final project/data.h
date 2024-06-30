b0281f1a4da0a04e39c342b6ef0d15269201e5d3
Mariam-Alashmawy <145690999+Mariam-Alashmawy@users.noreply.github.com>
2024-06-30 04:49:27 +0300

    Update data.h

--- final project/data.h
+++ final project/data.h
@@ -3,6 +3,16 @@
 
 #define MAX_NAME_LEN 100
 
+typedef struct ReservedBook {
+    struct Book *book; 
+    struct ReservedBook *next; 
+} ReservedBook;
+
+typedef struct BorrowedBook {
+    struct Book *book; 
+    struct BorrowedBook *next; 
+} BorrowedBook;
+
 typedef struct Book {
     char name[MAX_NAME_LEN];
     char author[MAX_NAME_LEN];
@@ -13,13 +23,14 @@ typedef struct Book {
 } Book;
 
 typedef struct User {
-    char username[MAX_NAME_LEN];
-    char password[MAX_NAME_LEN];
     int user_id;
-    int borrowed_books[2]; // IDs of borrowed books
-    int borrow_count;
-    char library_card_id[MAX_NAME_LEN];
-    float balance;
+    char username[50];
+    char password[50];
+    int isAdmin;
+    int library_card_id[10];
+    int borrowed_count; 
+    struct BorrowedBook *borrowed_books;
+    struct ReservedBook *reserved_books; 
     struct User *next;
 } User;
 
@@ -32,6 +43,19 @@ typedef struct Admin {
     struct Admin *next;
 } Admin;
 
+typedef struct BorrowRequest {
+    User *user;    
+    Book *book;     
+    int status;    
+    struct BorrowRequest *next; 
+} BorrowRequest;
+
+typedef struct NewBookRequest {
+    char book_name[MAX_NAME_LEN];
+    char book_author[MAX_NAME_LEN];
+    struct NewBookRequest *next;
+} NewBookRequest;
+
 extern Book *book_head;
 extern User *user_head;
 extern Admin *admin_head;
@@ -40,4 +64,4 @@ void init_data();
 void load_data();
 void save_data();
 
-#endif // DATA_H
+#endif 
